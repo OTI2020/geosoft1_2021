19b018c5421f6eae200d5b4fb62b24e9758cf020
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-05-01 22:04:03 +0200

    take away the main

--- Uebung_2/uebung_2.js
+++ Uebung_2/uebung_2.js
@@ -2,26 +2,7 @@
  * just for better overview
  * @author @OTI2020 Gustav
  * @version 0.7.4 - capitulation
- * @function main
  */
-main() //runner for main
-function main() {    
-    var separationArray = makeSeparationArray(route, polygon)
-    console.log(separationArray)
-    // console.log("separationArray " + separationArray)
-    
-    var sectionArray = sectionCount(separationArray)
-    console.log(sectionArray)
-    // console.log("sectionArray " + sectionArray)
-    
-    var distArray = sectionSizeCount(sectionArray, route, separationArray)
-    console.log(distArray)
-    // console.log("distArray " + distArray)
-
-    var sortedArray = bubbleSort(distArray)
-    console.log(sortedArray)
-    // console.log("sortedArray " + sortedArray)
-}
 
 
 /**
