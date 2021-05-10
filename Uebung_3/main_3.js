d4497d7834c6063de05c26ac9e0468d20e033881
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-05-10 22:23:58 +0200

    testing onclick functionality

--- Uebung_3/main_3.js
+++ Uebung_3/main_3.js
@@ -1,9 +1,24 @@
+
+
 function get_input() {
     try {
-        var api_key = document.getElementById("input_field_api_key").value
-        console.log(api_key)
+        var API_key = document.getElementById("input_field_api_key").value
+        console.log(API_key)
 
     } catch (error) {
         window.alert("Please insert your correct API-key")
     }
 }
+
+
+function get_geolocation() {
+    try {
+        var geolocation = 9090
+        console.log("my position 🌐	" + geolocation)
+    } catch (error) {
+        window.alert("Please insert your correct API-key")
+    }
+}
+/**
+ * http://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}&appid={API_key}
+ */
