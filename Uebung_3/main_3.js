3b8eed2137613e5e4612e27f4d8adcb415de65ec
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-05-11 22:37:45 +0200

    catch edge cases

--- Uebung_3/main_3.js
+++ Uebung_3/main_3.js
@@ -15,45 +15,50 @@ function get_xhr_object(in_lat, in_long) {
     function statechangecallback() {
         //console.dir(x);
         if (x.status == "200" && x.readyState == 4) {
-        //console.log(x.responseText);
-        console.log(x)
-        console.log(x.responseText);
-        // document.getElementById("content").innerHTML = x.responseText +  JSON.stringify(JSON.parse(x.responseText),null,4);
-        show_weather(x.responseText)
+            //console.log(x.responseText);
+            // console.log("test_1");
+            show_weather(x.responseText)
         }
     }
 
     function errorcallback(e) {
         // console.dir(x);
         // console.dir(e);
+        // console.log("test_2");
         document.getElementById("content").innerHTML = "errorcallback: check web-console";
     }
 
     function loadcallback() {
-        //console.dir(x);
+        // console.dir(x);
+        // console.log("test_3");
         console.log(x.status);
     }    
 }
 
 
 function get_geolocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-      } else {
-          window.alert("Please insert your correct API-key")
-      }
+    if (document.getElementById("input_field_api_key").value != "") {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+            // console.log("test_4");
+        } else {
+            window.alert("Please try again")
+            // console.log("test_5");
+        }
     
-    function showPosition(position) {
-        // console.log("test showPosition()");
-        // console.dir(position)
-        
-        get_xhr_object(position.coords.latitude, position.coords.longitude)
-    }
+        function showPosition(position) {
+            // console.dir(position)
+            // console.log("test_6");
+            get_xhr_object(position.coords.latitude, position.coords.longitude)
+        }
     
-    function showError(error) {
-        console.dir(error);
-        window.alert(error + " Please insert your correct API-key")
-      
+        function showError(error) {
+            // console.log("test_7");
+            console.dir(error);
+            window.alert("Please agree the browser getting your position - it is necessary " + error + " RUN AGAIN")
+        }   
+    } else {
+        window.alert("Please insert API-key")
     }
 }
 
@@ -66,8 +71,12 @@ function show_weather(weather_json) {
     //generate table_1
     // fill table column header
     var row = table_1.insertRow()
-    row.insertCell().innerHTML = weather_json.main.temp + " Fahrenheit"
-    row.insertCell().innerHTML = weather_json.main.feels_like + " Fahrenheit"    
-    row.insertCell().innerHTML = weather_json.main.humidity + " %"       
-    row.insertCell().innerHTML = weather_json.main.pressure + " Pa"    
+    row.insertCell().innerHTML = weather_json.name
+    row.insertCell().innerHTML = weather_json.main.temp
+    row.insertCell().innerHTML = weather_json.main.feels_like
+    row.insertCell().innerHTML = weather_json.main.humidity        
+    row.insertCell().innerHTML = weather_json.main.pressure   
+    row.insertCell().innerHTML = weather_json.weather[0].description
+    
+    // `https://api.openweathermap.org/img/wn/${weather_json.weather[0].icon}@2x.png`
 }
