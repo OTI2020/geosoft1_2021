7369e086b8091ee3a51d59c0e313fab900f0e7e7
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-05-11 17:18:12 +0200

    add show_weather()

--- Uebung_3/main_3.js
+++ Uebung_3/main_3.js
@@ -9,16 +9,25 @@ function get_input() {
         window.alert("Please insert your correct API-key")
     }
 }
-
+console.log(document)
 
 function get_geolocation() {
     try {
-        var geolocation = 9090
+        var geolocation = document.getElementById("timezone")
+        geolocation = 9090
         console.log("my position 🌐	" + geolocation)
     } catch (error) {
-        window.alert("Please insert your correct API-key")
+        window.alert("Please search for location again")
     }
 }
 /**
- * http://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}&appid={API_key}
+ * api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
  */
+
+function show_weather(weather_json) {
+    document.getElementsById("date").innerHTML= "date: " + weather_json.current.dt
+    document.getElementsById("temp").innerHTML= "temparature: " + weather_json.current.temp + " celsius"    
+    document.getElementsById("felt_temp").innerHTML= "felt temparature: " + weather_json.current.feels_like + " celsius"    
+    document.getElementsById("humidity").innerHTML= "humidity: " + weather_json.current.humidity + " %"       
+    document.getElementsById("speed").innerHTML= "wind speed: " + weather_json.current.temp + " m/s"    
+}
