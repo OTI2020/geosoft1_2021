36a130adea2bd21c19d382bb38a68d75da60fa1b
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-05-11 18:27:55 +0200

    add xhr functionality & fix showWeather()

--- Uebung_3/main_3.js
+++ Uebung_3/main_3.js
@@ -1,33 +1,87 @@
 
+function get_xhr_object(in_lat, in_long) {
+
+    var in_API_key = get_input()
+    var resource = "https://api.openweathermap.org/data/2.5/weather?lat="+in_lat+"&lon="+in_long+"&appid="+in_API_key
+
+    var x = new XMLHttpRequest();
+    x.onload = loadcallback;
+    x.onerror = errorcallback;
+    x.onreadystatechange = statechangecallback;
+    x.open("GET", resource, true);
+    x.send();
+
+
+    function statechangecallback() {
+        //console.dir(x);
+        if (x.status == "200" && x.readyState == 4) {
+        //console.log(x.responseText);
+        console.log(x)
+        console.log(x.responseText);
+        // document.getElementById("content").innerHTML = x.responseText +  JSON.stringify(JSON.parse(x.responseText),null,4);
+        show_weather(x.responseText)
+        }
+    }
+
+    function errorcallback(e) {
+        // console.dir(x);
+        // console.dir(e);
+        document.getElementById("content").innerHTML = "errorcallback: check web-console";
+    }
+
+    function loadcallback() {
+        //console.dir(x);
+        console.log(x.status);
+    }    
+}
+
+
 
 function get_input() {
     try {
         var API_key = document.getElementById("input_field_api_key").value
         console.log(API_key)
+        return API_key
 
     } catch (error) {
         window.alert("Please insert your correct API-key")
     }
 }
-console.log(document)
+
 
 function get_geolocation() {
-    try {
-        var geolocation = document.getElementById("timezone")
-        geolocation = 9090
-        console.log("my position 🌐	" + geolocation)
-    } catch (error) {
-        window.alert("Please search for location again")
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+      } else {
+        // document.getElementById("location_div").innerHTML = "Geolocation N/A...";
+      }
+    
+    function showPosition(position) {
+        console.log("test showPosition");
+        console.dir(position)
+        console.log(position.coords.latitude)
+
+        get_xhr_object(position.coords.latitude, position.coords.longitude)
+      // document.getElementById("location_div").innerHTML = position.coords.latitude + ", " + position.coords.longitude;
+    }
+    
+    function showError(error) {
+      console.dir(error);
+      // document.getElementById("location_div").innerHTML = error.message;
     }
 }
-/**
- * api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
- */
+
 
 function show_weather(weather_json) {
-    document.getElementsById("date").innerHTML= "date: " + weather_json.current.dt
-    document.getElementsById("temp").innerHTML= "temparature: " + weather_json.current.temp + " celsius"    
-    document.getElementsById("felt_temp").innerHTML= "felt temparature: " + weather_json.current.feels_like + " celsius"    
-    document.getElementsById("humidity").innerHTML= "humidity: " + weather_json.current.humidity + " %"       
-    document.getElementsById("speed").innerHTML= "wind speed: " + weather_json.current.temp + " m/s"    
+    weather_json = JSON.parse(weather_json)
+    console.log(weather_json)
+
+    var table_1 = document.getElementById("table_1")
+    //generate table_1
+    // fill table column header
+    var row = table_1.insertRow()
+    row.insertCell().innerHTML = weather_json.main.temp + " Fahrenheit"
+    row.insertCell().innerHTML = weather_json.main.feels_like + " Fahrenheit"    
+    row.insertCell().innerHTML = weather_json.main.humidity + " %"       
+    row.insertCell().innerHTML = weather_json.main.pressure + " Pa"    
 }
