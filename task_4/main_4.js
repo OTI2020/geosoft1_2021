8ca36a365b2a6f6a988fbe90d532a2daa02f7afe
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-05-20 18:08:45 +0200

    change zoom level

--- task_4/main_4.js
+++ task_4/main_4.js
@@ -2,18 +2,19 @@
 
 main()
 function main(){
-    var map=L.map('map').setView([51, 9], 4)
+    var map=L.map('map').setView([51, 9], 5)
     L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=JetIUYq8eZdZDelkpBpy', {
         attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
     }).addTo(map)
 
-    var markerGEO1 = L.marker([51.969, 7.596]).addTo(map)
-    console.log("markerGEO1")
-    console.log(markerGEO1)
+    var markerGEO1 = L.marker([51.969, 7.596])
+    markerGEO1.addTo(map)
+    // console.log("markerGEO1")
+    // console.log(markerGEO1)
 
     var myRoute = L.geoJSON(routeVar).addTo(map)
-    console.log("myRoute")
-    console.log(myRoute)
+    // console.log("myRoute")
+    // console.log(myRoute)
 }
 
 
