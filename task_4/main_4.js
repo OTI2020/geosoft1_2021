33c27b6cfc071374ec20248995cef4120a73c3df
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-05-25 12:36:02 +0200

    split main more functions and trying to use turf

--- task_4/main_4.js
+++ task_4/main_4.js
@@ -1,15 +1,23 @@
 
 
+
 main()
-function main(){
+function main() {
+    create_map_with_route_and_marker()
+    insec_poly = get_toolbar_functionality()
+    get_intersection_points(myRoute, insec_poly)
+}
+
+
+
+function create_map_with_route_and_marker() {
     var map=L.map('map').setView([51, 9], 5)
 
-    
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: 18,
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors",
         id: "osm"
-    }).addTo(map);
+    }).addTo(map)
 
     var markerGEO1 = L.marker([51.969, 7.596])
     markerGEO1.addTo(map)
@@ -21,16 +29,18 @@ function main(){
     myRoute.addTo(map)
     // console.log("myRoute")
     // console.log(myRoute)
+}
 
 
-    ////////////////////////////
-    // here draw plugin beginns
-    ////////////////////////////
-    
-    
+
+////////////////////////////
+// here draw plugin beginns
+////////////////////////////
+
+function get_toolbar_functionality() {
     var drawnItems = L.featureGroup().addTo(map)
-    console.log(drawnItems);
-    console.log(L.Control.Draw);
+    console.log(drawnItems)
+    // console.log(L.Control.Draw)
 
 
     map.addControl(new L.Control.Draw( {
@@ -49,9 +59,35 @@ function main(){
         }
     }))
 
-map.on(L.Draw.Event.CREATED, function (event) {
-    var layer = event.layer;
-    drawnItems.addLayer(layer);
-})
+    map.on(L.Draw.Event.CREATED, function (e) {
+        // var type = e.layerType
+        var layer = e.layer
+        // add this layer to drawnItems
+        drawnItems.addLayer(layer)
+        console.log(drawnItems.toGeoJSON())
+        
+        // to prevent that more than one polygone gets created
+        // this toolbar functionality gets hidden
+        $(".leaflet-draw-toolbar-top").css("visibility", "hidden")
+    })
+
+    map.on('draw:edited', function (e) {
+        var layers = e.layers
+        layers.eachLayer(function (layer) {
+        })
+    })
+
+    var polygon_for_intersection = drawnItems.toGeoJSON()
+    return polygon_for_intersection
+
+}   
+    
+
+
+function get_intersection_points(in_line_1, in_line_2) {
+    var intersects = turf.lineIntersect(in_line_1, in_line_2)
+    console.log(intersects)
+    return intersects
 }
+
 
