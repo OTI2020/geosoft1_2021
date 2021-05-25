4f9340731990572738fa0b9e5e1becd6ca9e6211
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-05-25 16:57:44 +0200

    solved marker problems - popups working

--- task_4/main_4.js
+++ task_4/main_4.js
@@ -3,9 +3,10 @@
 
 main()
 function main() {
-    create_map_with_route_and_marker()
-    insec_poly = get_toolbar_functionality()
-    get_intersection_points(myRoute, insec_poly)
+    var start_map = create_map_with_route_and_marker()
+    // var insec_points = 
+    get_toolbar_functionality(start_map)
+    // insec_points
 }
 
 
@@ -29,21 +30,26 @@ function create_map_with_route_and_marker() {
     myRoute.addTo(map)
     // console.log("myRoute")
     // console.log(myRoute)
+
+    return map
 }
 
 
 
-////////////////////////////
-// here draw plugin beginns
-////////////////////////////
-
-function get_toolbar_functionality() {
-    var drawnItems = L.featureGroup().addTo(map)
+/**
+ * here draw plugin beginns
+ * @function get_toolbar_functionality()
+ * @param {Leaflet-Map} in_map 
+ * @returns intersects - is a geojson that contains drawn rectangle
+ */
+function get_toolbar_functionality(in_map) {
+    var drawnItems = L.featureGroup().addTo(in_map)
+    console.log("drawnItems")
     console.log(drawnItems)
     // console.log(L.Control.Draw)
 
 
-    map.addControl(new L.Control.Draw( {
+    in_map.addControl(new L.Control.Draw( {
         edit: {
             featureGroup: drawnItems,
             poly: {
@@ -59,35 +65,83 @@ function get_toolbar_functionality() {
         }
     }))
 
-    map.on(L.Draw.Event.CREATED, function (e) {
+    
+    in_map.on(L.Draw.Event.CREATED, function (e) {
         // var type = e.layerType
         var layer = e.layer
         // add this layer to drawnItems
         drawnItems.addLayer(layer)
+        console.log("drawnItems.toGeoJSON()")
         console.log(drawnItems.toGeoJSON())
         
         // to prevent that more than one polygone gets created
         // this toolbar functionality gets hidden
         $(".leaflet-draw-toolbar-top").css("visibility", "hidden")
+
+        var polygon_for_intersection = drawnItems.toGeoJSON()
+        var out_points = get_intersection_points(routeVar, polygon_for_intersection)
+        console.log("out_points")
+        console.log(out_points)
+
+        create_weather_popups(in_map, out_points)
     })
 
-    map.on('draw:edited', function (e) {
+    in_map.on('draw:edited', function (e) {
         var layers = e.layers
         layers.eachLayer(function (layer) {
         })
     })
-
-    var polygon_for_intersection = drawnItems.toGeoJSON()
-    return polygon_for_intersection
-
 }   
     
 
 
+// called by get_toolbar_functionality in case of an drawn.event
 function get_intersection_points(in_line_1, in_line_2) {
     var intersects = turf.lineIntersect(in_line_1, in_line_2)
+    console.log("intersects");
     console.log(intersects)
     return intersects
 }
 
+
+// called by get_toolbar_functionality in case of an drawn.event
+function create_weather_popups(in_map, in_points) {
+    var number_of_insections = in_points.features.length
+    console.log(number_of_insections + "number_of_insections");
+    
+    for (let i=0; i<number_of_insections; i++) {
+        
+        var weather_marker = L.marker([in_points.features[i].geometry.coordinates[1], in_points.features[i].geometry.coordinates[0]])
+        weather_marker.addTo(in_map)
+
+        // rounded (2 decimal places) coordinates for the popup
+        var x_coor = Math.round(in_points.features[i].geometry.coordinates[1]*100)/100
+        var y_coor = Math.round(in_points.features[i].geometry.coordinates[0]*100)/100
+        
+        // marker names
+        var m_name = 'marker_' + (i+1)
+        weather_marker.bindPopup("<b> This is " + m_name + "<br /> coordinates: (" + x_coor + ", " + y_coor + ") <br />").openPopup()
+
+    }
+    
+}
+
+
+
+
+/*
+let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lng + "&exclude=minutely,hourly,daily&appid=" + weatherApi+"&units=metric"
+    $.ajax({
+        url: url,
+        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        success: function(result){
+            console.log(result.current.temp);
+            let weather = "<b>current Weather:</b><br>" +result.current.weather[0].description + "<br>" + result.current.temp + "°C";
+            console.log(weather);
+            marker.bindPopup(weather).openPopup();
+        }
+    }).done(function() {
+        $( this ).addClass( "done" );
+      });
+*/
 
