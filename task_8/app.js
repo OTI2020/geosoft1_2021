ac8f88838394f609d318b9b91d53ff986f26dd69
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-07-03 12:10:23 +0200

    vue computed ... first try

--- task_8/app.js
+++ task_8/app.js
@@ -3,13 +3,19 @@ console.log(this.atag);
 new Vue({
     el: '#app',
     data: {
+        firstname: '',
+        lastname: '',
         names: ['Florian', 'Gustav', 'Peter'],
         persons: [
             {name: 'Florian', age: 21},
             {name: 'Gustav', age: 53},
         ]
     },
-    methods: {},
+    computed: {
+        fullname() {
+            return `${this.firstname} ${this.lastname}`;
+        }
+    },
 });
 
 
