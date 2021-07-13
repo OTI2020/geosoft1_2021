b17efc7cc10a200b677eff85b875bb9402008169
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-07-13 17:20:05 +0200

    update instances

--- task_8/app.js
+++ task_8/app.js
@@ -1,37 +1,22 @@
-// we save dynamic data and can manupulate it later
-let vOne = new Vue({
-    el: '#app', // like an id
+var title = new Vue({
+    el: '#headings',
     data: {
-        username: '',
-        password: '',
-        currentUser: 'Florian',
-        feedback: '',
-        users: [
-            {name:'Florian', password:'1234'},
-            {name:'Gustav', password:'gut'},
-            {name:'gfreiher', password:'sehrgut'},
-            {name:'halloTuer', password:'qwert'},
-        ]
-    },
-    methods: {
-        checkPassword(){
-            console.log("halloTimo");
-            console.log(this.users.filter(user => password === user.password));
-            return this.users.filter(user => password === user.password)
-        },
-        checkCredentials(event){
-            event.preventDefault()
-            if (
-                this.username === this.currentUser &&
-                this.checkPassword(this.password).length !== 0
-            ){
-                this.feedback = 'correct credentials'
-            } else {
-                this.feedback = 'wrong credentials'
-            }
-        },
-        changeUser(name){
-            this.currentUser = name
-        }
+        message: 'Geosoftware 1, 2021 - Task 8'
     }
-});
+})
+
+//Subtitle vue
+var subtitle = new Vue({
+    el: '#subtitle',
+    data: {
+        message: 'Tobias B., Dominik Z. und Gustav v.A.'
+    }
+})
+
+//Table vue
+var table = new Vue({
+    el:'#table',
+    data:{
+        distances: distances
+    },
+})
